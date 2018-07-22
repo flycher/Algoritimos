ce6158ffc60c191367875e833b06fecb8d09eee9
Matheus Xavier Sampaio <matheus.sampaio011@gmail.com>
2018-07-22 12:14:44 -0300

    representacao de grafos

---
+++ EDA/Grafos/lista_de_adjacencia.h
@@ -0,0 +1,92 @@
+#include <list>
+
+class ListaDeAdjacencia
+{
+private:
+    int V; //quatidade de vertices
+    int E; //quatidade de arestas
+    bool directed; //se o grafo e direcionado
+    bool weighted; //se as arestas possuem peso
+    std::list<std::pair<int, int> > *G; //representação do grafo
+
+public:
+    ListaDeAdjacencia(int V, int E, bool directed, bool weighted);
+    ~ListaDeAdjacencia();
+    void addEdge(int u, int v, int weight); //adiciona uma aresta
+    void removeEdge(int u, int v); //remove uma aresta
+    bool checkEdge(int u, int v); //verifica se a aresta existe
+};
+
+ListaDeAdjacencia::ListaDeAdjacencia(int V, int E, bool directed, bool weighted)
+{
+    this->V = V;
+    this->E = 0;
+    this->directed = directed;
+    this->weighted = weighted;
+    G = new std::list<std::pair<int, int> >[V];
+}
+
+ListaDeAdjacencia::~ListaDeAdjacencia()
+{
+    for(int i = 0; i < V; i++)
+        G[i].clear();
+    delete [] G;
+}
+
+
+void ListaDeAdjacencia::addEdge(int u, int v, int weight = 1)
+{
+    G[u].push_back(std::make_pair(v, weight));
+    if(!directed)
+        G[v].push_back(std::make_pair(u, weight));
+
+    E++;
+};
+
+void ListaDeAdjacencia::removeEdge(int u, int v)
+{
+    std::list<std::pair<int, int> >::iterator it = G[u].begin();
+
+    while(it != G[u].end())
+    {
+        if(it->first == v)
+        {
+            G[u].erase(it);
+            break;
+        }
+        it++;
+    }
+
+    if(!directed)
+    {
+        it = G[v].begin();
+
+        while(it != G[v].end())
+        {
+            if(it->first == u)
+            {
+                G[v].erase(it);
+                break;
+            }
+            it++;
+        }
+    }
+
+    E--;
+};
+
+bool ListaDeAdjacencia::checkEdge(int u, int v)
+{
+    std::list<std::pair<int, int> >::iterator it = G[u].begin();
+
+    while(it != G[u].end())
+    {
+        if(it->first == v)
+        {
+            return true;
+        }
+        it++;
+    }
+
+    return false;
+};
---
+++ EDA/Grafos/matriz_de_adjacencia.h
@@ -0,0 +1,133 @@
+#include <climits>
+
+class MatrizDeAdjacencia
+{
+private:
+    int V; //quatidade de vertices
+    int E; //quatidade de arestas
+    bool directed; //se o grafo e direcionado
+    bool weighted; //se as arestas possuem peso
+    int **G; //representação do grafo
+
+public:
+    MatrizDeAdjacencia(int V, int E, bool directed, bool weighted);
+    ~MatrizDeAdjacencia();
+    void addEdge(int u, int v, int weight); //adiciona uma aresta
+    void removeEdge(int u, int v); //remove uma aresta
+    bool checkEdge(int u, int v); //verifica se a aresta existe
+};
+
+MatrizDeAdjacencia::MatrizDeAdjacencia(int V, int E, bool directed, bool weighted)
+{
+    this->V = V;
+    this->E = 0;
+    this->directed = directed; //se não for direcinado, cria matriz triangular
+    this->weighted = weighted; //se possui peso, não presença da aresta é representada por infinito(INT_MAX)
+
+    G = new int*[V];
+    for(int i = 0; i < V; i++)
+    {
+        if(!directed)
+        {
+            G[i] = new int[i + 1];
+            if(weighted)
+            {
+                for(int j = 0; j < i + 1; j++)
+                    G[i][j] = INT_MAX;
+            }
+        }
+        else
+        {
+            G[i] = new int[V];
+            if(weighted)
+            {
+                for(int j = 0; j < V; j++)
+                    G[i][j] = INT_MAX;
+            }
+        }
+    }
+};
+
+MatrizDeAdjacencia::~MatrizDeAdjacencia()
+{
+    for(int i = 0; i < V; i++)
+        delete [] G[i];
+    delete [] G;
+};
+
+void MatrizDeAdjacencia::addEdge(int u, int v, int weight = 1)
+{
+    if(directed)
+        G[u][v] = weight;
+    else
+    {
+        if(v > u)
+            G[v][u] = weight;
+        else
+            G[u][v] = weight;
+    }
+
+    E++;
+};
+
+void MatrizDeAdjacencia::removeEdge(int u, int v)
+{
+    if(directed)
+    {
+        if(weighted)
+            G[u][v] = INT_MAX;
+        else
+            G[u][v] = 0;
+    }
+    else
+    {
+        if(weighted)
+        {
+            if(v > u)
+                G[v][u] = INT_MAX;
+            else
+                G[u][v] = INT_MAX;
+        }
+        else
+        {
+            if(v > u)
+                G[v][u] = 0;
+            else
+                G[u][v] = 0;
+        }
+    }
+
+    E--;
+};
+
+bool MatrizDeAdjacencia::checkEdge(int u, int v)
+{
+    if(weighted)
+    {
+        if(directed)
+        {
+            return G[u][v] != INT_MAX;
+        }
+        else
+        {
+            if(v > u)
+                return G[v][u] != INT_MAX;
+            else
+                return G[u][v] != INT_MAX;
+        }
+    }
+    else
+    {
+        if(directed)
+        {
+            return G[u][v];
+        }
+        else
+        {
+            if(v > u)
+                return G[v][u];
+            else
+                return G[u][v];
+        }
+    }
+};
